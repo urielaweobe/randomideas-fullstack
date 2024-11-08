d8bbb40724a9483669c8f9887483ca7aba80418b
urielaweobe <urielaweobe@gmail.com>
2024-11-08 10:19:01 +0100

    fix: change base url, build app, remove font awesome and use cdn to reduce sizing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,37 +1,40 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, '../public'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "../public"),
+    filename: "bundle.js",
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, '../public'),
+      directory: path.resolve(__dirname, "../public"),
     },
     port: 3000,
     open: true,
     hot: true,
     compress: true,
     historyApiFallback: true,
+    proxy: {
+      "/api": "http://localhost:5500",
+    },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
@@ -39,9 +42,9 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack App',
-      filename: 'index.html',
-      template: './src/index.html',
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./src/index.html",
     }),
     new MiniCssExtractPlugin(),
   ],
